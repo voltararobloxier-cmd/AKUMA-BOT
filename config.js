c7ac24ad4f7ea8e21f907cba1e9319fc2d6cb018
voltararobloxier-cmd <voltararobloxier@gmail.com>
2025-11-04 15:18:54 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ module.exports = {
   SESSION_ID: "PASTE_SESSION_ID_HERE", //VOTTRE SESSION ID ICI
   botName: "Okarun Bot",
   ownerName: "♱ 𝓓𝓝𝓢 ♱ dev",
-  ownerNumber: "201105950400", // Le numéro du propriétaire
+  ownerNumber: "22879694279", // Le numéro du propriétaire
   prefix: "+",
   repo: "https://github.com/Bsteph12/AKUMA-BOT.git", // Le lien de votre dépôt
-  mode: "public" ,// ou "private"
+  mode: "private" ,// ou "private"
   newsletterJid: "120363419336864081@newsletter",
   IMGBB_API_KEY: "254b685aea07ed364f7091dee628d26b"
 };
